46ea41d318a044432bc209ccf25b5984bea8590a
Sokol Paja <kolpaja28@gmail.com>
2022-05-08 14:23:36 +0200

    fix main branch to be clean only with dev dependecies of tailwin

--- src/App.js
+++ src/App.js
@@ -1,76 +1,9 @@
-import { useState } from 'react';
-import StepperContext from './context/StepperContext'
+import React from 'react'
 
-import Stepper from './components/Stepper.jsx'
-import StepperControl from './components/StepperControl.jsx';
-import AccountInfo from './components/steps/AccountInfo.jsx';
-import PersonalDetails from './components/steps/PersonalDetails.jsx';
-import Final from './components/steps/Final';
-
-function App() {
-  const [currentStep, setCurrentStep] = useState(1)
-  const [userData, setUserData] = useState('')
-  const [finalData, setFinalData] = useState(null)
-
-  const steps = [
-    'Account Information',
-    'Personal Details',
-    'Final'
-  ];
-
-  const resetForm = () => {
-    setUserData('');
-    setFinalData(null)
-    setCurrentStep(1)
-  }
-
-  const displayStep = (step) => {
-    switch (step) {
-      case 1:
-        return <AccountInfo />
-      case 2:
-        return <PersonalDetails />
-      case 3:
-        return <Final resetForm={resetForm} />
-      default:
-        break;
-    }
-  }
-
-  const handleClick = (direction) => {
-    let newStep = currentStep;
-
-    direction === 'next' ? newStep++ : newStep--
-    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep)
-  }
-
+const App = () => {
   return (
-    <div className="md:w-1/2  mx-auto shadow-xl rounded-3xl pb-2 bg-white">
-      {/* stepper comp */}
-      <div className='container horizontal mt-5'>
-        <Stepper currentStep={currentStep} steps={steps} />
-
-        {/* display the steps */}
-        <div className='p-10 my-4'>
-          <StepperContext.Provider value={{
-            step: currentStep,
-            userData,
-            setUserData,
-            finalData,
-            setFinalData
-          }}>
-            {displayStep(currentStep)}
-          </StepperContext.Provider>
-        </div>
-      </div>
-      {/* navigation controller */}
-      {currentStep !== steps.length && <StepperControl
-        handleClick={handleClick}
-        currentStep={currentStep}
-        steps={steps}
-      />}
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/context/StepperContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const StepperContext = createContext(null)
-
-export default StepperContext
